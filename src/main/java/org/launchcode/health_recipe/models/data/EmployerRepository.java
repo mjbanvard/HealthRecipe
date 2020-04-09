84cdef0eb884d27af653f2e3470639932819c229
Gerard Darris <darrisgr@gmail.com>
2020-04-09 06:26:55 -0500

    Deleted EmployerRepository.java, JobRepository.java, SkillRepository.java; not relevant to project

--- src/main/java/org/launchcode/health_recipe/models/data/EmployerRepository.java
+++
@@ -1,9 +0,0 @@
-//package org.launchcode.health_recipe.models.data;
-//
-//import org.launchcode.health_recipe.models.Employer;
-//import org.springframework.data.repository.CrudRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface EmployerRepository extends CrudRepository<Employer, Integer> {
-//}
