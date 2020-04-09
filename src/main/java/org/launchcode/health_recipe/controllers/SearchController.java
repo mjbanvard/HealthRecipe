7053ab34c801e81039ff991cb0db03cbfc8f2333
Gerard Darris <darrisgr@gmail.com>
2020-04-09 04:18:25 -0500

    Edited SearchController.java to search for recipes based off keyword search matching with recipe names

--- src/main/java/org/launchcode/health_recipe/controllers/SearchController.java
+++ src/main/java/org/launchcode/health_recipe/controllers/SearchController.java
@@ -1,44 +1,44 @@
-//package org.launchcode.health_recipe.controllers;
-//
-//import org.launchcode.health_recipe.models.Job;
-//import org.launchcode.health_recipe.models.JobData;
-//import org.launchcode.health_recipe.models.data.JobRepository;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.*;
-//
-//
-//import static org.launchcode.health_recipe.controllers.RecipeController.columnChoices;
-//
-///**
-// * Created by LaunchCode
-// */
-//@Controller
-//@RequestMapping("search")
-//public class SearchController {
-//
-//    @Autowired
-//    private JobRepository jobRepository;
-//
-//    @RequestMapping("")
-//    public String search(Model model) {
-//        model.addAttribute("columns", columnChoices);
-//        return "search";
-//    }
-//
-//    @PostMapping("results")
-//    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
-//        Iterable<Job> jobs;
-//        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
-//            jobs = jobRepository.findAll();
-//        } else {
-//            jobs = JobData.findByColumnAndValue(searchType, searchTerm, jobRepository.findAll());
-//        }
-//        model.addAttribute("columns", columnChoices);
-//        model.addAttribute("title", "Jobs with " + columnChoices.get(searchType) + ": " + searchTerm);
-//        model.addAttribute("jobs", jobs);
-//
-//        return "search";
-//    }
-//}
+package org.launchcode.health_recipe.controllers;
+
+import org.launchcode.health_recipe.models.Recipe;
+import org.launchcode.health_recipe.models.RecipeData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.launchcode.health_recipe.models.data.RecipeRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+
+import static org.launchcode.health_recipe.controllers.RecipeController.columnChoices;
+
+/**
+ * Created by LaunchCode
+ */
+@Controller
+@RequestMapping(value = "search")
+public class SearchController {
+
+    @Autowired
+    private RecipeRepository recipeRepository;
+
+    @RequestMapping("")
+    public String search(Model model) {
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
+
+    @PostMapping("results")
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm){
+        Iterable<Recipe> recipes;
+        if (searchTerm.toLowerCase().equals("all") || searchTerm.equals("")){
+            recipes = recipeRepository.findAll();
+        } else {
+            recipes = RecipeData.findByColumnAndValue(searchType, searchTerm, recipeRepository.findAll());
+        }
+        model.addAttribute("columns", columnChoices);
+        model.addAttribute("title", "Recipes with " + columnChoices.get(searchType) + ": " + searchTerm);
+        model.addAttribute("recipes", recipes);
+
+        return "search";
+    }
+}
