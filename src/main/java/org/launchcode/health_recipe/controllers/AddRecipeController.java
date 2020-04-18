9b50112b72c10afd917f8269c47335377391a763
James <jamesr817@gmail.com>
2020-04-18 07:58:37 -0500

    Going with back plan. Adding a seperate page for adding ingredients. Commented out previous code just for now.

--- src/main/java/org/launchcode/health_recipe/controllers/AddRecipeController.java
+++ src/main/java/org/launchcode/health_recipe/controllers/AddRecipeController.java
@@ -1,9 +1,7 @@
 package org.launchcode.health_recipe.controllers;
 
 
-import org.launchcode.health_recipe.models.Ingredient;
 import org.launchcode.health_recipe.models.Recipe;
-import org.launchcode.health_recipe.models.data.IngredientRepository;
 import org.launchcode.health_recipe.models.data.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -12,38 +10,44 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 
 @Controller
+@RequestMapping
 public class AddRecipeController {
 
     @Autowired
     private RecipeRepository recipeRepository;
 
-    @Autowired
-    private IngredientRepository ingredientRepository;
+//    @Autowired
+//    private IngredientRepository ingredientRepository;
 
     @GetMapping("add")
     public String displayAddRecipeForm(Model model) {
         model.addAttribute("title", "Add Recipe");
-        model.addAttribute("recipe", recipeRepository.findAll());
-        model.addAttribute("ingredient", ingredientRepository.findAll());
+//        model.addAttribute("recipe", recipeRepository.findAll());
+//        model.addAttribute("ingredient", ingredientRepository.findAll());
         model.addAttribute(new Recipe());
-        model.addAttribute(new Ingredient());
+//        model.addAttribute(new Ingredient());
         return "add";
     }
 
     @PostMapping("add")
     public String processAddRecipeForm(@ModelAttribute @Valid Recipe newRecipe,
+//                                       @ModelAttribute @Valid Ingredient newIngredient,
                                        Errors errors, Model model){
 
         if (errors.hasErrors()) {
-            model.addAttribute("title", "Add Recipe");
+//            model.addAttribute("title", "Add Recipe");
             return "add";
         }
 
         recipeRepository.save(newRecipe);
-        return "redirect:";
+//        ingredientRepository.save(newIngredient);
+//        return "redirect:";
+        return "ingredient/add";
     }
+
 }
