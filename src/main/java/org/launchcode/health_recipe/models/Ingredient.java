f00bef700127f5b0affdb611c39193d828cdfbdf
Michael Banvard <mjbanvard@gmail.com>
2020-04-20 16:44:05 -0500

    Working on getting MySQL to instantiate online, through Heroku.
    Worked a little on the wrong branch, but all will be well.

--- src/main/java/org/launchcode/health_recipe/models/Ingredient.java
+++ src/main/java/org/launchcode/health_recipe/models/Ingredient.java
@@ -1,23 +1,60 @@
 package org.launchcode.health_recipe.models;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
-public class Ingredient extends AbstractEntity {
+public class Ingredient {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @NotNull
+    @Size(max = 150)
+//    @ManyToOne()
+    private String name;
 
     @NotBlank(message = "Please provide Recipe ingredient.")
-    @Size(min = 3,max = 255, message = "Ingredient must be only 3-255 characters.  Please try again.")
-    private String ingredient;
+    @Size(min = 3,max = 150, message = "Ingredient must be only 3-150 characters.  Please try again.")
+    @JoinColumn(name = "ingredients")
+    public String ingredient;
 
+//    @ManyToOne
+//    @JoinColumn(/*name="recipe_name",*/ nullable=false)
+//    protected Recipe recipe;
 
-    public Ingredient(String ingredient){
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public Ingredient(int id, String name, String ingredient){
+
+        this.id = id;
+        this.name = name;
         this.ingredient = ingredient;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public Ingredient() {}
@@ -28,5 +65,31 @@ public class Ingredient extends AbstractEntity {
 
     public void setIngredient(String ingredient) {
         this.ingredient = ingredient;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return id == that.id &&
+                name.equals( that.name ) &&
+                ingredient.equals( that.ingredient );
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash( id, name, ingredient );
+    }
+
+    @ManyToOne(optional = false)
+    private Recipe recipes;
+
+    public Recipe getRecipes() {
+        return recipes;
+    }
+
+    public void setRecipes(Recipe recipes) {
+        this.recipes = recipes;
     }
 }
